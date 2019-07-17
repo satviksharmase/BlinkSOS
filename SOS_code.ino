97818373231db9a86489abd0055d482ae2ffba3c
satviksharmase <52960097+satviksharmase@users.noreply.github.com>
2019-07-17 11:57:21 +1000

    Add files via upload

---
+++ SOS_code.ino
@@ -0,0 +1,58 @@
+
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop() {
+ //for S
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000); 
+  //for O
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, HIGH);    
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, HIGH);    
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, HIGH);    
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000); 
+  //for S
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);                             
+}
